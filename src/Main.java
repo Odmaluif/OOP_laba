a81c7073eb2fa972bf0d04fa00c2cc22e31fad60
Odmaluif <dsvshsh3@gmail.com>
2025-11-02 22:34:25 +0400

    5.0

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,6 @@ import java.util.DuplicateFormatFlagsException;
 public class Main {
     public static void main(String[] args) throws IOException, DuplicateModelNameException, ClassNotFoundException, CloneNotSupportedException {
 
-        Car brand = new Car("Model", 6);
-        Vehicle bike = new Bike("Model", 6);
-
-        Vehicle clone = (Vehicle) brand.clone();
-
-        System.out.println("Перевод в строку оригинала: " + brand.toString());
-        System.out.println("Перевод в строку клона: " + clone.toString());
-        System.out.println(brand.equals(clone));
-
-        System.out.println("Хэш код оригинала: " + brand.hashCode());
-        System.out.println("Хэш код клона: " + clone.hashCode() );
-
-        clone.addModel("Model 23", 2321);
-        System.out.println("добавлена модель в клон");
-
-        System.out.println("Перевод в строку оригинала: " + brand.toString());
-        System.out.println("Перевод в строку клона: " + clone.toString());
-        System.out.println(brand.equals(clone));
-
-        System.out.println("Хэш код оригинала: " + brand.hashCode());
-        System.out.println("Хэш код клона: " + clone.hashCode() );
-
+        
     }
 }
